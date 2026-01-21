ec9485ec37870ad08964160aa06be7abd5f63a13
rinarudhei <rinaldiadrian5@gmail.com>
2026-01-21 11:55:13 +0700

    chore: translate design system to tailwind utilities

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const nunito = Nunito({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: "swap", // 'swap' ensures a fallback font is displayed while Nunito loads
+  variable: "--font-nunito", // Define a CSS variable name
 });
 
 export const metadata: Metadata = {
@@ -24,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${nunito.className} antialiased bg-accent-red`}>
         {children}
       </body>
     </html>
